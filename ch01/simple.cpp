53494591f94558180c5d7a83adfafc28da8a7640
jtdv01 <jtdv01@gmail.com>
2018-11-24 14:05:39 +1100

    init

---
+++ ch01/simple.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x,y;
+    cout << "Hello world. Enter 2 numbers number:" << endl;
+    cout << "x:";
+    cin >> x;
+    cout << "y:";
+    cin >> y;
+    int sum = x + y;
+    cout << "Sum is " << sum << endl;
+    return 0;
+}
