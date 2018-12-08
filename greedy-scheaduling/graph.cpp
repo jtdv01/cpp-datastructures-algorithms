7587b8020ab45c864ec97d132e11c71409986df9
jtdv <jtdv01@gmail.com>
2018-12-08 18:36:53 +1100

    Defined graph header

--- greedy-scheaduling/graph.cpp
+++ greedy-scheaduling/graph.cpp
@@ -1,57 +1,37 @@
 #include <iostream>
 #include <vector>
-
+#include "graph.h"
 using namespace std;
-struct Edge
+
+
+// Pass by reference &edges
+Graph::Graph(vector<Edge> const &edges, int N)
 {
-    int src, dest;
-};
+    adjList.resize(N);
 
-class Graph
+    for(auto &edge: edges)
+    {
+        // Append an extra 
+        // adjList[edge.src].push_back(edge.dest);
+        
+        // alternate way, use a pointer
+        auto srcList = &adjList[edge.src];
+        srcList->push_back(edge.dest);
+    }
+}
+
+void Graph::printGraph()
 {
-    public:
-        vector<vector <int> > adjList;
-
-        // Pass by reference &edges
-        Graph(vector<Edge> const &edges, int N)
-        {
-            adjList.resize(N);
-
-            for(auto &edge: edges)
-            {
-                // Append an extra 
-                // adjList[edge.src].push_back(edge.dest);
-                
-                // alternate way, use a pointer
-                auto srcList = &adjList[edge.src];
-                srcList->push_back(edge.dest);
-            }
-        }
-};
-
-void printGraph(Graph const& graph, int N)
-{
+    int N = adjList.size();
     for (int i = 0; i < N; i++)
     {
         // print current vertex number
         cout << i << " --> ";
  
         // print all neighboring vertices of vertex i
-        for (int v : graph.adjList[i])
+        for (int v : adjList[i])
             cout << v << " ";
         cout << endl;
     }
 }
 
-int main(){
-    vector<Edge> edges = {
-        { 0, 1 }, { 1, 2 }, { 2, 0 }, { 2, 1 },
-        { 3, 2 }, { 4, 5 }, { 5, 4 }
-    };
-
-    int N = edges.size();
-
-    Graph g = Graph(edges, N);
-
-    printGraph(g, N);
-}
